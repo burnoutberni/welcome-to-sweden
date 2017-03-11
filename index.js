fab6a8d79fa7636d54205abca47ca01fe165b405
burnoutberni <hello@nini.su>
2017-03-11 19:24:13 +0100

    rewrite

--- index.js
+++ index.js
@@ -1,188 +1,112 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const sendMessage = require(__dirname + '/sendMessages.js')
-const question = require(__dirname + '/questions.js')
-const information = require(__dirname + '/information.js')
-const app = express()
-const token = process.env.FB_PAGE_ACCESS_TOKEN
+const BootBot = require('bootbot');
+const languages = require(__dirname + '/ISOlanguage.json')
+
+const bot = new BootBot({
+  accessToken: process.env.FB_PAGE_ACCESS_TOKEN,
+  verifyToken: process.env.FB_VERIFY_TOKEN,
+  appSecret: process.env.FB_APP_SECRET
+});
 
 let users = {}
 
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', (req, res) => {
-  res.send('Hello world, I am a chat bot')
-})
-
-// for Facebook verification
-app.get('/webhook/', (req, res) => {
-  if (req.query['hub.verify_token'] === 'hack_for_sweden_team_open_sunshine') {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, wrong token')
-})
-
-const processMessage = (sender, event, user) => {
-  if (event.message && event.message.text) {
-    let text = event.message.text
-    console.log(text)
-    if (text.toLowerCase() === 'restart') {
-      user.lastStep = 'FIRST_MESSAGE'
-      event.postback = undefined
-    }
-    switch (user.lastStep) {
-      case 'FIRST_MESSAGE':
-        user.lastStep = 'FIRST_QUESTION'
-        sendMessage.text(sender, "Hej du!", () => {
-          question.role(sender)
-        })
-        return
-      case 'FIRST_QUESTION':
-        if (text.toLowerCase().indexOf('migrant') !== -1) {
-          user.lastStep = 'INTRO_MIGRANT'
-          user.type = 'MIGRANT'
-          sendMessage.text(sender, "Welcome to Sweden.", () => {
-            question.purpose(sender)
-          })
-          return
-        } else if (text.toLowerCase().indexOf('swede') !== -1) {
-          user.lastStep = 'INTRO_SWEDE'
-          user.type = 'SWEDE'
-          sendMessage.text(sender, "Cool. We are looking for buddies that we can match up with newly arrived people to Sweden.", () => {
-            question.consent(sender)
-          })
-          return
-        }
-        sendMessage.text(sender, "Sorry, I didn't understand.", () => {
-          question.role(sender)
-        })
-        return
-      case 'INTRO_MIGRANT':
-        if (text.toLowerCase().indexOf('info') !== -1
-          || text.toLowerCase().indexOf('job') !== -1
-        ) {
-          information(sender, event, user)
-          return
-        } else if (text.toLowerCase().indexOf('buddy') !== -1) {
-          user.lastStep = 'MIGRANT_BUDDY'
-          sendMessage.text(sender, "We are going to find a buddy for you that will help you with finding your daily routine, but first we need a couple of informations about you.", () => {
-            question.language(sender, user.language.join(' and '))
-          })
-          return
-        }
-        sendMessage.text(sender, "Sorry, I didn't understand.", () => {
-          question.purpose(sender)
-        })
-        return
-      case 'MIGRANT_BUDDY':
-        if (text.toLowerCase().indexOf('no') !== -1
-          || text.toLowerCase().indexOf('nej') !== -1
-        ) {
-          sendMessage.text(sender, "Okay, no problem. We also need your location, so we can find a buddy close to you.", () => {
-            question.location(sender)
-          })
-          return
-        }
-        sendMessage.text(sender, "Sorry, I didn't understand.", () => {
-          question.language(sender, user.language.join(' and '))
-        })
-        return
-    }
-  }
-
-  if (event.postback && event.postback.payload) {
-    let payload = event.postback.payload
-    switch (payload) {
-      case 'INTRO_MIGRANT':
-        user.lastStep = 'INTRO_MIGRANT'
-        user.type = 'MIGRANT'
-        sendMessage.text(sender, "Welcome to Sweden.", () => {
-          question.purpose(sender)
-        })
-        return
-      case 'MIGRANT_BUDDY':
-        user.lastStep = 'MIGRANT_BUDDY'
-        sendMessage.text(sender, "We are going to find a buddy for you that will help you with finding your daily routine, but first we need a couple of informations about you.", () => {
-          question.language(sender, user.language.join(' and '))
-        })
-        return
-      case 'MIGRANT_INFORMATION':
-        user.lastStep = 'MIGRANT_INFORMATION'
-        information(sender, event, user)
-        return
-      case 'INTRO_SWEDE':
-        user.lastStep = 'INTRO_SWEDE'
-        user.type = 'SWEDE'
-        sendMessage.text(sender, "Cool. We are looking for buddies that we can match up with newly arrived people to Sweden.", () => {
-          question.consent(sender)
-        })
-        return
-    }
-
-    if (payload.startsWith('MIGRANT_LANGUAGE_')) {
-      let language = payload.split('MIGRANT_LANGUAGE_')[1].toLowerCase()
-      if (!user.language.find(userLanguage => userLanguage === language)) { user.language.push(language) }
-      user.lastStep = 'MIGRANT_LANGUAGE'
-      sendMessage.text(sender, "Cool. We also need your location, so we can find a buddy close to you.", () => {
-        question.location(sender)
-      })
-    }
-
-    if (payload.startsWith('INFO_')) {
-      information(sender, event, user)
-    }
-  }
-
-  if (event.message
-    && event.message.attachments
-    && event.message.attachments[0].payload
-    && event.message.attachments[0].payload.coordinates
-  ) {
-    user.lastStep = 'MIGRANT_LOCATION'
-    console.log(event.message)
-    user.location = event.message.attachments[0].payload.coordinates
-    sendMessage.text(sender, "Awesome. We will send you a message when we found your buddy!", () => console.log('the end \\o/'))
-  }
+const askRole = (convo) => {
+  convo.ask((convo) => {
+    const buttons = [
+      { type: 'postback', title: `I'm a migrant`, payload: 'INTRO_MIGRANT'},
+      { type: 'postback', title: `I'm a Swede`, payload: 'INTRO_SWEDE'}
+    ]
+    convo.sendButtonTemplate(`Did you recently come to Sweden or are you a native?`, buttons)
+  }, (payload) => {
+    console.log(payload)
+  }, [
+    {
+      event: 'postback:INTRO_MIGRANT',
+      callback: (payload, convo) => {
+        convo.set('role', 'migrant')
+        convo.say('Welcome to Sweden.').then(() => askPurpose(convo));
+      }
+    },
+    {
+      event: 'postback:INTRO_SWEDE',
+      callback: (payload, convo) => {
+        convo.set('role', 'swede')
+        convo.say('Cool. We are looking for buddies that we can match up with newly arrived people to Sweden.').then(() => askConsent(convo));
+      }
+    },
+  ])
 }
 
-// do things
-app.post('/webhook/', (req, res) => {
-  let messaging_events = req.body.entry[0].messaging
-  messaging_events.map((event) => {
-    let sender = event.sender.id
-    let user
-    if (!users[sender]) {
-      sendMessage.userdata(sender, (fbUser) => {
-        fbUser = JSON.parse(fbUser)
-        users[sender] = {
-          firstName: fbUser.first_name,
-          lastName: fbUser.last_name,
-          language: [fbUser.locale ? fbUser.locale.split('_')[0] : undefined],
-          lastStep: 'FIRST_MESSAGE',
-        }
-        user = users[sender]
-        console.log('fetched fb user', fbUser, user)
-        processMessage(sender, event, user)
-      });
-    } else {
-      user = users[sender]
-      console.log('fetched user', user)
-      processMessage(sender, event, user)
-    }
-  })
-  res.sendStatus(200)
-})
+const askPurpose = (convo) => {
+  convo.ask((convo) => {
+    const buttons = [
+      { type: 'postback', title: `Job information`, payload: 'MIGRANT_INFORMATION'},
+      { type: 'postback', title: `Find me a buddy`, payload: 'MIGRANT_BUDDY'}
+    ]
+    convo.sendButtonTemplate(`Do you want to find information about job seeking in Sweden or do you want to be connected to a buddy?`, buttons)
+  }, (payload) => {
+    console.log(payload)
+  }, [
+    {
+      event: 'postback:MIGRANT_INFORMATION',
+      callback: (payload, convo) => {
+        // todo
+      }
+    },
+    {
+      event: 'postback:MIGRANT_BUDDY',
+      callback: (payload, convo) => {
+        convo.say('We are going to find a buddy for you that will help you with finding your daily routine, but first we need a couple of informations about you.').then(() => askLanguage(convo));
+      }
+    },
+  ])
+}
 
-// Spin up the server
-app.listen(app.get('port'), () => {
-  console.log('running on port', app.get('port'))
+const askLanguage = (convo) => {
+  const englishLanguageNames = languages.filter((language) => language.code === convo.get('languages'))
+
+  convo.ask(`Do you speak any other language than ${englishLanguageNames.join(' and ')}?`, (payload, convo, data) => {
+    const text = payload.message.text.toLowerCase()
+    const addedLanguage = languages.find((language) => {
+      return language.name.toLowerCase() === text
+        || language.nativeName.toLowerCase() === text
+    })
+    convo.set('language', [convo.get('language'), addedLanguage.code])
+    convo.say(`Cool. We also need your location, so we can find a buddy close to you`).then(() => askLocation(convo))
+  })
+}
+
+const askLocation = (convo) => {
+  convo.ask({
+    text: 'Where do you live?',
+    quickReplies: {"content_type": "location"}
+  }, (payload) => {
+    console.log(payload)
+  }, [
+    {
+      event: 'quick_reply',
+      callback: (payload, convo) => {
+        console.log(payload, convo)
+        convo.say(`Ok, here's what you told me about you:
+        - Name: ${convo.get('name')}
+        - Favorite Food: ${convo.get('food')}
+        - Gender: ${convo.get('gender')}
+        - Age: ${convo.get('age')}`).then(() => {
+          convo.say('I will send you a message when we found your buddy!')
+          convo.end()
+        })
+      }
+    }
+  ])
+}
+
+bot.hear('hello', (payload, chat) => {
+  chat.getUserProfile().then((user) => {
+    chat.conversation((convo) => {
+      convo.set('first_name', user.first_name)
+      convo.set('languages', user.locale.split('_')[0])
+      chat.say(`Hello, ${user.first_name}!`).then(() => askRole(convo))
+    })
+  })
 })
