d952180cb46d2d628fabcc9605803af9409d35fc
burnoutberni <hello@nini.su>
2017-03-11 15:30:48 +0100

    add information and users and stuff

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const sendMessage = require(__dirname + '/sendMessages.js')
 const question = require(__dirname + '/questions.js')
+const information = require(__dirname + '/information.js')
 const app = express()
 const token = process.env.FB_PAGE_ACCESS_TOKEN
 
-let users = []
+let users = {}
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
 
 // for Facebook verification
 app.get('/webhook/', (req, res) => {
-  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+  if (req.query['hub.verify_token'] === 'hack_for_sweden_team_open_sunshine') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
@@ -36,15 +37,15 @@ app.post('/webhook/', (req, res) => {
   let messaging_events = req.body.entry[0].messaging
   messaging_events.map((event) => {
     let sender = event.sender.id
-    let user = users.find((user) => user.fbId === sender)
+    let user = users[sender]
     if (!user) {
       sendMessage.userdata(sender, (user) => {
-        users.push({
+        users[sender] = {
           firstName: user.first_name,
           lastName: user.last_name,
-          fbId: sender,
-          lastStep: 0,
-        })
+          language: [],
+          lastStep: 'FIRST_MESSAGE',
+        }
       });
     }
     console.log(event)
@@ -52,7 +53,8 @@ app.post('/webhook/', (req, res) => {
     if (event.message && event.message.text) {
       let text = event.message.text
       switch (user.lastStep) {
-        case 0:
+        case 'FIRST_MESSAGE':
+          user.lastStep = 'FIRST_QUESTION'
           sendMessage.text(sender, "Hej du!", () => {
             sendMessage.button(sender, "Did you recently come to Sweden or are you a native?", [{
               "type":"postback",
@@ -86,13 +88,28 @@ app.post('/webhook/', (req, res) => {
       if (payload === 'INTRO_MIGRANT') {
         user.lastStep = 'INTRO_MIGRANT'
         sendMessage.text(sender, "Welcome to Sweden.", () => {
+          sendMessage.button(sender, "Do you want to find information about job seeking in Sweden or do you want to be connected to a buddy?", [{
+            "type": "postback",
+            "title": "Job information",
+            "payload": "MIGRANT_INFORMATION"
+          }, {
+            "type": "postback",
+            "title": "Find me a buddy",
+            "payload": "MIGRANT_BUDDY"
+          }
+          }])
           sendMessage.text(sender, "We are going to find a buddy for you that will help you with finding your daily routine, but first we need a couple of informations about you.", () => {
             question.language(sender)
           })
         })
         return
       }
+      if (payload === 'MIGRANT_INFORMATION') {
+        information()
+      }
       if (payload.startsWith('MIGRANT_LANGUAGE_')) {
+        let language = payload.split('MIGRANT_LANGUAGE_')[1]
+        if (!user.language.find(language)) { user.language.push(language) }
         user.lastStep = 'MIGRANT_LANGUAGE'
         sendMessage.text(sender, "Cool. We also need your location, so we can find a buddy close to you.", () => {
           question.location(sender)
