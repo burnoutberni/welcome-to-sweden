1fb4c2b3b8bf8d17594610003b0d20bf9e6b1b0d
Pierre <pierre.mesure@gmail.com>
2017-03-11 14:36:52 +0100

    Added class files

---
+++ person.js
@@ -0,0 +1,8 @@
+module.exports = function Person(firstName, lastName, position, languages) {
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        position: position,
+        languages: languages
+    }
+}
