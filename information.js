08ef9bcc0be5eee0ebcf7ac6b3f546109d3da4d0
Pierre <pierre.mesure@gmail.com>
2017-03-11 17:19:13 +0100

    Minor changes with the questions

--- information.js
+++ information.js
@@ -3,22 +3,24 @@ const question = require(__dirname + '/questions.js')
 const questionsInfo = require(__dirname + '/questionsInfo.js')
 
 module.exports = function(sender, event, user) {
-    
-    sendMessage.text(sender, "Sure, no problem!", () => {
-          questionsInfo.service(sender)
-        })
-    
+
     if (event.postback && event.postback.payload) {
         let payload = event.postback.payload
         
         switch (payload) {
-          case 'INFO_JOB':
-            user.lastStep = 'MIGRANT_INFORMATION';
-            sendMessage.text(sender, "Hey, my name is Hans. I'll be your job counselor today", () => {
-                questionsInfo.jobService(sender);
+            case 'INFO_MIGRANT':
+                user.lastStep = 'INTRO_MIGRANT';
+                sendMessage.text(sender, "Sure, no problem!", () => {
+                    questionsInfo.service(sender)
             })
-          return
-        case 'INFO_JOB_CLOSEST_AGENCY':
+            return
+            case 'INFO_JOB':
+                user.lastStep = 'MIGRANT_INFORMATION';
+                sendMessage.text(sender, "Hey, my name is Hans. I'll be your job counselor today", () => {
+                    questionsInfo.jobService(sender);
+                })
+            return
+            case 'INFO_JOB_CLOSEST_AGENCY':
             user.lastStep = 'JOB_INFORMATION';
 
         }
