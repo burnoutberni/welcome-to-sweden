01d5378cddcd38eb2af9fdf2d8e46c25ed259791
burnoutberni <hello@nini.su>
2017-03-11 16:30:28 +0100

    move some questions and get more data

--- questions.js
+++ questions.js
@@ -1,5 +1,29 @@
 const sendMessage = require(__dirname + '/sendMessages.js')
 
+const askRole = (sender) => {
+  sendMessage.button(sender, "Did you recently come to Sweden or are you a native?", [{
+    "type":"postback",
+    "title":"I'm a migrant!",
+    "payload":"INTRO_MIGRANT"
+  }, {
+    "type":"postback",
+    "title":"I'm a Swede!",
+    "payload":"INTRO_SWEDE"
+  }])
+}
+
+const askPurpose = (sender) => {
+  sendMessage.button(sender, "Do you want to find information about job seeking in Sweden or do you want to be connected to a buddy?", [{
+    "type": "postback",
+    "title": "Job information",
+    "payload": "MIGRANT_INFORMATION"
+  }, {
+    "type": "postback",
+    "title": "Find me a buddy",
+    "payload": "MIGRANT_BUDDY"
+  }])
+}
+
 const askLanguage = (sender, language) => {
   sendMessage.button(sender, `It seems like you speak ${language}, do you also want to select a second language that you feel comfortable speaking in?`, [{
     "type":"postback",
@@ -29,6 +53,8 @@ const askConsent = (sender) => {
 }
 
 module.exports = {
+  role: askRole,
+  purpose: askPurpose,
   language: askLanguage,
   location: askLocation,
   consent: askConsent,
